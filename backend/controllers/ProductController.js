0ebfbe61e99c96ab2f3293791985d2d97b43a406
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-27 09:06:38 +0700

    created method updateProduct

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -51,8 +51,46 @@ export const saveProduct=(req,res) =>{
 }
 
 
-export const updateProduct=(req,res) =>{
+export const updateProduct=async (req,res) =>{
+    const product=await Product.findOne({
+        where:{
+            id:req.params.id
+        }
+    });    
+    if(!product) return res.status(404).json({msg:"Data not Found!."})  
     
+    let fileName="";
+    if(req.files === null){
+        fileName=Product.image;
+    }else{
+        const file=req.files.file;
+        const fileSize=file.data.length;
+        const ext=path.extname(file.name);
+        const allowedType=[".png",".jpg",".jpeg"];
+        fileName=file.md5 + ext;
+        
+        if(!allowedType.includes(ext.toLocaleLowerCase())) return res.status(422).json({msg:"Invalid type image!."});
+        if(fileSize > 5000000) return res.status(422).json({msg:"Image must be less than 5 MB!."});
+        
+        const filepath=`./public/images/${product.image}`;
+        fs.unlinkSync(filepath);
+    
+        file.mv(`./public/images/${fileName}`, (err) =>{
+            if(err) return res.status(500).json({msg:err.messsage})
+        });
+    }
+    const name=req.body.title;
+    const url=`${req.protocol}://${req.get("host")}/images/${fileName}`;
+    try{
+        await Product.update({name, image:fileName, url},{
+            where:{
+                id:req.params.id
+            }
+        })
+        res.status(200).json({msg:"Product updated successfuly!."});
+    }catch(error){
+        console.log(error.message);
+    }
 }
 
 export const deleteProduct=async (req,res) =>{
@@ -61,7 +99,7 @@ export const deleteProduct=async (req,res) =>{
             id:req.params.id
         }
     });    
-    if(!product) return res.status(404).json({msg:"No Data Found!."})
+    if(!product) return res.status(404).json({msg:"Data not Found!."})
     try{
         const filepath=`./public/images/${product.image}`;
         fs.unlinkSync(filepath);
