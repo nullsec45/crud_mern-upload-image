1a1fd1170b9e1f9655d4b6b5c07a90f9bb6b5fe4
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-27 18:52:36 +0700

    create frontend

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import {Link} from "react-router-dom";
+
+const ProductList = () => {
+	const [products, setProducts]=useState([]);
+
+	useEffect(() => {
+			getProducts();
+	}, []);
+
+	const getProducts=async() =>{
+		const response=await axios.get("http://localhost:5000/products");
+
+		setProducts(response.data);
+	}
+
+	const deleteProduct=async (productId) =>{
+		try{
+			await axios.delete(`http://localhost:5000/products/${productId}`);
+			getProducts();
+		}catch(error){
+			console.log(error.message);
+		}
+	}
+	return (
+		<div className="container mt-5">
+			<Link to="/add" className="button is-success">Add New</Link>
+			<div className="columns is-multiline mt-2">
+				{products.map((product)=>(
+				  <div className="column is-one-quarter" key={product.id}>
+					<div class="card">
+					  <div class="card-image">
+					    <figure class="image is-4by3">
+					      <img src={product.url} alt="Placeholder image"/>
+					    </figure>
+					  </div>
+					  <div class="card-content">
+					    <div class="media">
+					      <div class="media-content">
+					        <p class="title is-4">{product.name}</p>
+					      </div>
+					    </div>
+					  </div>
+
+					  <footer className="card-footer">
+					  	<Link to={`edit/${product.id}`} className="card-footer-item">Edit</Link>
+					  	<a onClick={() => deleteProduct(product.id)} className="card-footer-item">Delete</a>
+					  </footer>
+					</div>
+				  </div>
+				))}
+				
+			</div>
+		</div>
+
+	)
+}
+
+export default ProductList;
